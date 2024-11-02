975980f4bc59bcc47a35d15de78f228320735030
Augusto Peniel <penielaugusto@hotmail.com>
2024-11-02 19:14:47 +0000

    Realizado declaração de variaveis e reailizado inicio de mensagens para coleta dos dados.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,11 +8,17 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    int codigo_cidade;
+    char nome_cidade[20];
+    int população_cidade;
+    float area_cidade;
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+    printf("Bem vindo ao Super trunfo de paises \n")
+    printf( " Vamos cadastrar nossas cartas ")
+
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
